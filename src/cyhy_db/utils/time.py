a2b798af25534264564709e4bf45a90f66c2491c
Felddy <markf@geekpad.com>
2024-04-22 11:43:50 -0400

    Improve comments on utcnow util

--- src/cyhy_db/utils/time.py
+++ src/cyhy_db/utils/time.py
@@ -3,5 +3,11 @@ from datetime import datetime, timezone
 
 
 def utcnow() -> datetime:
-    """Returns a timezone-aware datetime object with the current time in UTC."""
+    """Returns a timezone-aware datetime object with the current time in UTC.
+
+    This is useful for default value factories in Beanie models.
+
+    Returns:
+        datetime: The current time in UTC
+    """
     return datetime.now(timezone.utc)
