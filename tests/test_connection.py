0afb2ed78179a838cdf29666785d0dc97f3633e7
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-10-01 16:41:32 -0400

    Add two unit tests for the initialize_db function
    Co-authored-by: Mark Feldhousen <mark.feldhousen@gwe.cisa.dhs.gov>

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,10 +1,14 @@
 """Test database connection."""
 
+# Standard Python Libraries
+from unittest.mock import AsyncMock, patch
+
 # Third-Party Libraries
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+import pytest
 
 # cisagov Libraries
-# from cyhy_db import initialize_db
+from cyhy_db.db import ALL_MODELS, initialize_db
 from cyhy_db.models import CVE
 
 
@@ -21,3 +25,40 @@ async def test_connection_beanie():
     # Attempt to find a document in the empty CVE collection
     result = await CVE.get("CVE-2024-DOES-NOT-EXIST")
     assert result is None, "Expected no document to be found"
+
+
+# Confused about the order of patch statements relative to the order of the test
+# function parameters?  See here:
+# https://docs.python.org/3/library/unittest.mock.html#patch
+@patch("cyhy_db.db.init_beanie", return_value=None)  # mock_init_beanie
+@patch("cyhy_db.db.AsyncIOMotorClient")  # mock_async_iomotor_client
+@pytest.mark.asyncio
+async def test_initialize_db_success(mock_async_iomotor_client, mock_init_beanie):
+    """Test a success case of the initialize_db function."""
+    db_uri = "mongodb://localhost:27017"
+    db_name = "test_db"
+
+    mock_client = AsyncMock()
+    mock_db = AsyncMock(AsyncIOMotorDatabase)
+    mock_client.__getitem__.return_value = mock_db
+    mock_async_iomotor_client.return_value = mock_client
+
+    db = await initialize_db(db_uri, db_name)
+    assert db == mock_db
+    mock_async_iomotor_client.assert_called_once_with(db_uri)
+    mock_init_beanie.assert_called_once_with(
+        database=mock_db, document_models=ALL_MODELS
+    )
+
+
+@pytest.mark.asyncio
+async def test_initialize_db_failure():
+    """Test a failure case of the initialize_db function."""
+    db_uri = "mongodb://localhost:27017"
+    db_name = "test_db"
+
+    with patch(
+        "cyhy_db.db.AsyncIOMotorClient", side_effect=Exception("Connection error")
+    ):
+        with pytest.raises(Exception, match="Connection error"):
+            await initialize_db(db_uri, db_name)
