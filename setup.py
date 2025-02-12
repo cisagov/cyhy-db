b122225ad402b326c28765e266febc8ca0025806
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-02-12 15:21:04 -0500

    Remove unnecessary types lib
    docopt is not used by this project.

--- setup.py
+++ setup.py
@@ -101,7 +101,6 @@ setup(
         # field of the mypy pre-commit hook to avoid discrepancies in type
         # checking between environments.
         "dev": [
-            "types-docopt",
             "types-setuptools",
         ],
         "test": [
